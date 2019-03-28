98f8044e186dd255e208c8afe507ca651e09afb0
Trystan Kaes <baby.hughee@gmail.com>
2019-03-27 19:14:51 -0600

    Added the client and server stuff to this thing

---
+++ NETWORKS/LAB2/client.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+#include <netdb.h>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
+
+
+void error(std::string msg){
+  cerr << (msg);
+  exit(1);
+}
+
+
+int main(){
+    /* -------declare all the neccesary integers.------- */
+  int listenSocket, msg_size;
+  socklen_t clientSize;
+  int inPort = 2414; //this is the port and also my student ID
+  char  buffer[256]; //this will hold the message recieved and sent.
+  struct sockaddr_in serverAddress; //server
+  struct hostent *server;
+
+    /* ---------Manage socket jazz--------- */
+  listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(listenSocket < 0) //Verify if succesful
+    error("Error opening socket"); //Well shoot.
+
+  server = gethostbyname("csegrid.ucdenver.edu");
+  if(server == NULL) //Verify if succesful
+    error("Error finding host"); //Well shoot.
+
+  bzero((char*) &serverAddress, sizeof(serverAddress)); //clearBuffer
+
+    /* ---------Get that stuffs told other stuffs--------- */
+  serverAddress.sin_family = AF_INET;  //declare addr family
+  bcopy((char *)server->h_addr, (char *)&serverAddress.sin_addr.s_addr, server->h_length);
+  serverAddress.sin_port = htons(inPort);  //declare Port number
+
+    /* ---------Tie it up in a bow--------- */
+  if (connect(listenSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
+     error("Error connecting"); //Well shoot.
+
+    /* ---------Listen--------- */
+  cout << "Please enter a message: ";
+  bzero(buffer, 256);
+  std::string temp;
+  std::getline(std::cin, temp);
+  buffer = temp;
+
+  msg_size = write(listenSocket, buffer, 255);
+  if(msg_size < 0) //Verify if succesful
+    error("Error writing"); //Well shoot.
+
+  bzero(buffer,256);
+    msg_size = read(listenSocket, buffer, 255);
+  if(msg_size < 0) //Verify if succesful
+    error("Error writing"); //Well shoot.
+
+  cout << "The message is: " << buffer << endl;
+
+    return 0;
+}
---
+++ NETWORKS/LAB2/server.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
+
+
+void error(std::string msg){
+  cerr << (msg);
+  exit(1);
+}
+
+
+int main(){
+    /* -------declare all the neccesary integers.------- */
+  int listenSocket, in_Connect, msg_size;
+  socklen_t clientSize;
+  int inPort = 2414; //this is the port and also my student ID
+  char  buffer[256]; //this will hold the message recieved and sent.
+  struct sockaddr_in serverAddress; //server
+  struct sockaddr_in clientAddress; //client
+
+    /* ---------Manage socket jazz--------- */
+  listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(listenSocket < 0) //Verify if succesful
+    error("Error opening socket"); //Well shoot.
+
+  bzero((char*) &serverAddress, sizeof(serverAddress)); //clearBuffer
+
+    /* ---------Get that server stuffs told other stuffs--------- */
+  serverAddress.sin_family = AF_INET;  //declare addr family
+  serverAddress.sin_port = htons(inPort);  //declare Port number
+  serverAddress.sin_addr.s_addr = INADDR_ANY;  //Set this to the local IP
+
+    /* ---------Tie it up in a bow--------- */
+  if (bind(listenSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
+     error("Error binding"); //Well shoot.
+
+    /* ---------Listen--------- */
+  listen(listenSocket, 5);
+
+  clientSize = sizeof(clientAddress);
+  in_Connect = accept(listenSocket,(struct sockaddr*) &clientAddress,&clientSize);
+  if(in_Connect < 0) //Verify if succesful
+    error("Error accepting"); //Well shoot.
+
+   /* ---------Get Message--------- */
+  bzero(buffer,256);
+  msg_size = read(in_Connect, buffer, 255);
+  if(msg_size < 0) //Verify if succesful
+    error("Error reading"); //Well shoot.
+  cout << "The message is: " << buffer << endl;
+
+    return 0;
+}
