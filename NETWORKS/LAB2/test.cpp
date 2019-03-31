cf7261b783fb755ddba50c8ef86e12e31faa8f50
Trystan Kaes <baby.hughee@gmail.com>
2019-03-31 16:59:04 -0600

    Finished make and added a 'test' line to the make reformatted test.cpp so it is ready to be integrated.

--- NETWORKS/LAB2/test.cpp
+++ NETWORKS/LAB2/test.cpp
@@ -6,34 +6,18 @@
 #include <cstring>
 #include <dirent.h>
 
-#define bzero(b,len) (memset((b), '\0', (len)), (void) 0); //maybe this works
-
 using std::cout;
 using std::cin;
 using std::endl;
 using std::cerr;
 
+std::string getCatalog();
+void error(std::string);
 
 int main(){
-    char cwd[PATH_MAX];
-    getcwd(cwd, sizeof(cwd));
-    std::string directory;
+    std::string directory = getCatalog();
 
-    DIR *dir;
-
-    if((dir = opendir(cwd)) == NULL){ cout << "CWD_NOT_FOUND";  }
-
-    struct dirent* i;
-
-    while((i = readdir(dir)) != NULL ){
-        directory.append(i->d_name);
-        directory.append("\n");
-    }
-
-    closedir(dir);
-
-
-     cout << directory << endl;
+    cout << directory << endl;
 
     return 0;
 }
@@ -44,25 +28,30 @@ void error(std::string msg){
 }
 
 
-
+/** Makes a list of every file in the working directory seperated by new lines
+  \@ Param Takes nothing
+  \@ Return a string **/
 std::string getCatalog(){
   char cwd[PATH_MAX];
-  getcwd(cwd, sizeof(cwd));
+  getcwd(cwd, sizeof(cwd)); //get the current working directory
   std::string directory;
 
-  DIR *dir;
+  DIR *dir; //prepare to open a stream.
 
-  if((dir = opendir(cwd)) == NULL){ cout << "CWD_NOT_FOUND";  }
+  if((dir = opendir(cwd)) == NULL){
+      cout << "CWD_NOT_FOUND";
+      return "CWD_NOT_FOUND";
+    } //if it doesnt exist output error and return error.
 
   struct dirent* i;
 
-  while((i = readdir(dir)) != NULL ){
-      directory.append(i->d_name);
-      directory.append("\n");
+  while((i = readdir(dir)) != NULL ){ //otherwise
+      directory.append(i->d_name); //add the new item to the string
+      directory.append("\n"); //and add a new line
   }
 
-  closedir(dir);
+  closedir(dir); //close the stream
 
 
-   return directory;
+   return directory; //and return
 }
