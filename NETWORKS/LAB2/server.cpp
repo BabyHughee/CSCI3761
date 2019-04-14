783dcdef92da41beed64c3b52995a35a140df0da
Trystan Kaes <baby.hughee@gmail.com>
2019-04-13 23:31:23 -0600

    forking

--- NETWORKS/LAB2/server.cpp
+++ NETWORKS/LAB2/server.cpp
@@ -73,8 +73,18 @@ int main(int argc, char* argv[]) {
 
 
 
-    /* ---------Listen--------- */
-  listen(listenSocket, 5);
+     /* ---------Listen--------- */
+   listen(listenSocket, 5);
+   printf("server: listening for incomming connections \n");
+
+    /*---------SIG handling---------*/
+   sa.sa_handler = sigchld_handler;
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = SA_RESTART;
+
+   if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+         perror("sigaction");
+         exit(1); }
 
   clientSize = sizeof(clientAddress);
 
