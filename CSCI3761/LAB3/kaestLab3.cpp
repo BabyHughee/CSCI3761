b4c5ca8024d8ca8d24ec2e38b29e8683add14995
Trystan Kaes <baby.hughee@gmail.com>
2019-05-02 13:25:15 -0600

    Project is done. going to test it on CSEgrid and start working on performance.

--- CSCI3761/LAB3/kaestLab3.cpp
+++ CSCI3761/LAB3/kaestLab3.cpp
@@ -1,13 +1,176 @@
 #include "kaestLab3.h"
 
 
+// struct connectNode& organizeTree(struct connectNode mst[], int &size){
+//   struct connectNode ordered[size];
+//   bool exit = false;
+//   int e = 0;
+//   int i = 0;
+//   while(!exit){
+//     for(int j = i; j < size; j++){
+//       if(ordered[e].node2 == mst[j].node1){
+//         ordered[++e] = mst[j];
+//         i++;
+//       }
+//       else{
+//         j++;
+//       }
+//
+//
+//     }
+//
+//     if(i == size){
+//       exit = true;
+//     }
+//   }
+//
+//   for(int q = 0; q < size; q++){
+//       std::cout << "(" << ordered[q].node1 << "," << ordered[q].node2 << ")" << std::endl;
+//     }
+// }
+
+
+
+
+/**A Utitlity function for the merge sort
+   *\param left is the left segmented node list
+   *\param right is the right segmented node list
+   *\return merged is the merging of the left and right node lists */
+std::vector<connectNode> SBWmerge(std::vector<connectNode> left, std::vector<connectNode> right)
+{
+   std::vector<connectNode> merged;
+   while ((int)left.size() > 0 || (int)right.size() > 0) { //we havent processed everything
+
+      if ((int)left.size() > 0 && (int)right.size() > 0) { //if each vector still has stuffs
+
+         if ((int)left.front().cost <= (int)right.front().cost) { //if the front of left is less that front of right
+            merged.push_back(left.front()); //take the smaller one ( left )
+            left.erase(left.begin());    } //and delete
+          else{
+            merged.push_back(right.front());  //take the smaller one ( right )
+            right.erase(right.begin());  } //and delete
+
+      }
+      else if ((int)left.size() > 0) { //if the left one still has stuffs (and the right is empty)
+
+            for (int i = 0; i < (int)left.size(); i++){   merged.push_back(left[i]);  } //put in the last of the left
+            break;
+
+      }
+      else if ((int)right.size() > 0) { //if the right one still has stuffs (and the left is empty)
+            for (int i = 0; i < (int)right.size(); i++){   merged.push_back(right[i]);  } //put in the last of the right
+            break;
+      }
+   }
+   return merged; //return the merged boi
+}
+
+
+
+
+
+
+
+
+/**Sort the vector structs by minimum weight to maximum using merge sort
+   *\param toSort is the vector that holds the weights
+   *\return result is the sorted array */
+std::vector<connectNode> sortByWeight(std::vector<connectNode> &toSort){
+
+     if (toSort.size() <= 1) //if there is nothing we just eturn it all
+        return toSort;
+
+     std::vector<connectNode> sorted, left, right; //we need the sorted one and the two halves
+
+     int middle = ((int)toSort.size()+ 1) / 2; //where are we splitting this one
+
+     //construct the left list (initial list from beginning to the middle)
+     for (int i = 0; i < middle; i++) {   left.push_back(toSort[i]);  }
+
+     //construct the right list (initial list from middle to the end
+     for (int i = middle; i < (int)toSort.size(); i++){    right.push_back(toSort[i]);   }
+
+     left = sortByWeight(left); //send the left back through (want to get to smallest vector)
+     right = sortByWeight(right); //send the right back through (want to get to smallest vector)
+     sorted = SBWmerge(left, right); //merge those two lists
+
+     return sorted; //return what was accomplished
+}
+
+/**Function to find a set of i
+  *\param subset is our list of good connections
+  *\param i is to be found
+  *\return return the parent of i */
+int find(struct subNet subsets[], int i)
+{
+    if (subsets[i].parent != i) //if the subset[i]'s parent isn't i
+        subsets[i].parent = find(subsets, subsets[i].parent); //set equal to the prior generations
+
+    return subsets[i].parent;
+}
+
+void Union(struct subNet subset[], int x, int y)
+{
+    int rootOfx = find(subset, x); //set x's root to the root of x
+    int rootOfy = find(subset, y); //set y's root to the root of y
+
+    if (subset[rootOfx].rank < subset[rootOfy].rank) //if the subset at x < y
+        subset[rootOfx].parent = rootOfy; //set y as parent
+    else if (subset[rootOfx].rank > subset[rootOfy].rank) //x > y
+        subset[rootOfy].parent = rootOfx; //set x as parent of y
+
+    else // x = y
+    {
+        subset[rootOfy].parent = rootOfx; //choose x at random
+        subset[rootOfx].rank++; //increase it's rank
+    }
+}
+
+/**The actual function to find the MST
+   *\param foundMST is going to be the result
+   *\param network is the list of all possible connections  */
+void findMST(struct connectNode result[], std::vector<connectNode> &network, int &size){
+
+      int e = 0;  // index for result
+      int i = 0;  // index for edges
+
+      //prepare memory
+      struct subNet *subnets =  (struct subNet*) malloc( size * sizeof(struct subNet) );
+
+      // create subnets
+      for (int i = 0; i < size; ++i)
+      {
+          subnets[i].parent = i;
+          subnets[i].rank = 0;
+      }
+
+      while (e < size - 1)// looking for size - 1 edges
+      {
+          //take the smallest non double edge
+          struct connectNode next_path = network[i];
+
+          i++; //increment
+
+          int x = find(subnets, next_path.node1); //find cycle
+          int y = find(subnets, next_path.node2); //find cycle
+
+          if (x != y) //if there is a cycle
+          {
+              result[e++] = next_path; //this is a good addition
+              Union(subnets, x, y); //add in what we found
+          }
+          //otheriwise that sucked. boo
+      }
+
+}
+
 /**Read in the network data line by line into an array
    *\param file is the file we are reading in from
    *\param input is the vector to put the network into
    *\param numberNodes is the amount of nodes in the network
    *\return error tracks if there was an error, what type, and where in the
                                                                           file.*/
-exception_status readInNetwork(std::string file, std::vector<connectNode> &input, int numberNodes){
+exception_status readInNetwork(std::string file, std::vector<connectNode> &input, int &numberNodes){
   exception_status error;
 
     std::ifstream infile;
--- CSCI3761/LAB3/kaestLab3_main.cpp
+++ CSCI3761/LAB3/kaestLab3_main.cpp
@@ -4,6 +4,15 @@
 
 
 int main(int argc, char* argv[]) {
+//Declare the variables needed
+std::vector<connectNode> network;
+int networkSize = 0;
+exception_status error;
+
+// int count = 1;
+
+
+
 if (argc != 2){
   std::cerr << "./MST [textfile]" << std::endl;
   exit(1);
@@ -14,23 +23,15 @@ if (argc != 2){
   /*-*/  gettimeofday(&start, NULL); /*-*/
   /*------------CLOCK STATUS------------*/
 
-//Declare the variables needed
-std::vector<connectNode> network;
-int networkSize = 0;
-exception_status error;
 
-//Read in stuff
+
+//Prepare for finding the minimal spanning tree
   error = readInNetwork(argv[1], network, networkSize); //read in the network
   if(error.error == true){ std::cout << error.status << std::endl; } //output error
+  connectNode foundMST[networkSize - 1];
+  network = sortByWeight(network); //sort the nodes by weight
 
-
-
-for (int i = 0; i < network.size(); i++){
-  std::cout << network[i].node1 << " ";
-  std::cout << network[i].node2 << " ";
-  std::cout << network[i].cost << " ";
-  std::cout << std::endl;
-}
+  findMST(foundMST, network, networkSize);
 
 
   /*--------------------------CLOCK STATUS-------------------------*/
@@ -41,5 +42,88 @@ for (int i = 0; i < network.size(); i++){
   /*-*/    std::cout << "Total Execution time = " << elapsed    /*-*/
   /*-*/              << " ms" << std::endl;                     /*-*/
   /*--------------------------CLOCK STATUS-------------------------*/
+
+int cost = 0;
+for (int i = 0; i < networkSize - 1; ++i){
+    cost += foundMST[i].cost;
+  }
+
+  std::cout << "Minimum Cost = " << cost << std::endl;
+
+
+  std::cout << "Minimum Spanning Tree (T) : " << std::endl;
+
+int j = 0;
+  for (int i = 1; i < networkSize - 1; ++i){
+
+      if (j == 8){
+        std::cout << '\n';
+        j = 0;
+      }
+
+      std::cout << "(" << foundMST[i].node1 << "," << foundMST[i].node2 << ")";
+      j++;
+
+      if(i != (networkSize - 2)){
+        std::cout << ", ";
+      }
+
+
+    }
+
+    std::cout << std::endl;
+
+
   return 0;
 }
+
+
+
+
+// for (int i = 0; i < network.size(); i++){
+//   std::cout << network[i].node1 << " ";
+//   std::cout << network[i].node2 << " ";
+//   std::cout << network[i].cost << " ";
+//   std::cout << std::endl;
+// }
+
+
+// for (int i = 0; i < network.size(); i++){
+//   std::cout << network[i].node1 << " ";
+//   std::cout << network[i].node2 << " ";
+//   std::cout << network[i].cost << " ";
+//   std::cout << std::endl;
+// }
+
+  // std::cout << std::endl;
+  // std::cout << std::endl;
+  // std::cout << std::endl;
+
+
+
+    // 66 - 77
+// for (int i = 0; i < networkSize - 1; ++i){
+// std::cout <<  foundMST[i].node1 << " - " << foundMST[i].node2 << std::endl;
+//   }
+
+
+
+// 66 - 77
+// for (int i = 1; i < networkSize - 1; ++i){
+//     weight += foundMST[i].cost;
+//     std::cout << ",";
+//     if (count == 8){
+//       std::cout << i;
+//       std::cout << std::endl;
+//       count = 0;
+//
+//     }
+//     std::cout << "(" << foundMST[i].node1 << "," << foundMST[i].node2 << ")";
+//
+//     count++;
+//   }
+  //
+  // std::cout << std::endl;
+  // std::cout << weight;
+  // std::cout << std::endl;
+  // std::cout << std::endl;
